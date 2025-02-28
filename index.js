85a1507d41b0e71c6269723ae9f427fc868acb3d
Nur Azis Saputra <nurazissaputra@gmail.com>
2025-02-28 16:39:26 +0700

    feat(api): find all data using API

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
+import express from 'express';
 import mongoose from 'mongoose';
 
+// Connection to Database
 mongoose.connect('mongodb://127.0.0.1:27017/testDB')
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Connection error:', err));
+
+// Schema for User
+const Schema = mongoose.Schema;
+const userSchema = new Schema({
+  name: { type: String, required: true },
+  age: { type: Number, required: true },
+  status: { type: String, default: "active" }
+})
+
+// Model
+const User = mongoose.model('User', userSchema);
+
+const app = express();
+app.use(express.json());
+
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json({data: users})
+  } catch (error) {
+    console.error('Error fetching users:', error);
+  }
+})
+
+app.listen(3000, '127.0.0.1', () => {
+  console.log('Running server at http://127.0.0.1:3000');
+});
